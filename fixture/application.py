5a7ca1be61747b5f40a85908112dc7cfb7bd6ea8
Kasia <kasia@air.hq.e-liberty.org>
2017-11-06 11:11:10 +0100

    Separate class to session management

--- fixture/application.py
+++ fixture/application.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
+from fixture.session import SessionHelper
 
 
 class Application:
@@ -6,13 +7,10 @@ class Application:
     def __init__(self):
         self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
+        self.session = SessionHelper(self)
 
     def destroy(self):
         self.wd.quit()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
 
     def return_to_groups_page(self):
         wd = self.wd
@@ -40,17 +38,6 @@ class Application:
     def open_groups_page(self):
         wd = self.wd
         wd.find_element_by_link_text("groups").click()
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
     def open_home_page(self):
         wd = self.wd
