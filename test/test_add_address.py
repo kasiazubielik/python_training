f128a895f733c80806ef544636ba8b985d37ca81
Kasia <kasia@air.hq.e-liberty.org>
2017-11-06 11:45:38 +0100

    Revert "Separated classes to work with group and addresses"
    This reverts commit 5ee3f9fd38b2b17202bac373f3a3e2fabd1853a4.

--- test/test_add_address.py
+++ test/test_add_address.py
@@ -14,16 +14,16 @@ def app(request):
     
 def test_add_address(app):
     app.session.login(username="admin", password="secret")
-    app.address.create(Address(firstname="Jan", middlename="P.", lastname="Kowalski", nickname="none", title="Mr.", company="none", address="ul. Polna 3 00-123 Warszawa",
-                               home_phone="+22 0987867", mobile_phone="+48 765890765", work_phone="+48 564389067", fax="none", email="jan.kowalski@mail.com",
-                               email2="jan.k@mymail.com", email3="j.kowalski.kowalski.com", homepage="www.jakowalski.com", birth_date="1981", address2="none", notes="none",
-                               phone2="home"))
+    app.create_address(Address(firstname="Jan", middlename="P.", lastname="Kowalski", nickname="none", title="Mr.", company="none", address="ul. Polna 3 00-123 Warszawa",
+                            home_phone="+22 0987867", mobile_phone="+48 765890765", work_phone="+48 564389067", fax="none", email="jan.kowalski@mail.com",
+                            email2="jan.k@mymail.com", email3="j.kowalski.kowalski.com", homepage="www.jakowalski.com", birth_date="1981", address2="none", notes="none",
+                            phone2="home"))
     app.session.logout()
 
 def test_add_empty_address(app):
     app.session.login(username="admin", password="secret")
-    app.address.create(Address(firstname="", middlename="", lastname="", nickname="", title="", company="", address="", home_phone="", mobile_phone="=", work_phone="", fax="", email="",
-                               email2="", email3="", homepage="", birth_date="", address2="", notes="", phone2=""))
+    app.create_address(Address(firstname="", middlename="", lastname="", nickname="", title="", company="", address="", home_phone="", mobile_phone="=", work_phone="", fax="", email="",
+                                  email2="", email3="", homepage="", birth_date="", address2="", notes="", phone2=""))
     app.session.logout()
 
 
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -14,12 +14,12 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.create(Group(name="new group", header="Logo", footer="Comment"))
+    app.create_group(Group(name="new group", header="Logo", footer="Comment"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.create(Group(name="", header="", footer=""))
+    app.create_group(Group(name="", header="", footer=""))
     app.session.logout()
 
